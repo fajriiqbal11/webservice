a5db48e333b4e7558b63d58cb6197210efdb6ee7
fajrimi <68212198+fajriiqbal11@users.noreply.github.com>
2020-10-16 12:55:49 +0700

    Add files via upload

--- customers.php
+++ customers.php
@@ -1,2 +1,41 @@
-# webservice
-12181609
+<?php
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET');
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "nortwind";
+try {
+	$conn = new PDO("mysql:host=$servername;dbname=$database",
+								$username,$password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE,
+						PDO::ERRMODE_EXCEPTION);			
+}catch(PDOException $e){
+	echo "connection failed :".$e->getMessag();
+}
+$sql = "SELECT * FROM customers";
+$data= $conn->prepare($sql);
+$data->execute();
+$products = [];
+while($row = $data->fetch(PDO::FETCH_OBJ)){
+	$customers[] = ["CustomerID"=>$row->CustomerID,
+                    "CompanyName"=>$row->CompanyName,
+                    "ContactName"=>$row->ContactName,
+                    "ContactTitle"=>$row->ContactTitle,
+                    "Address"=>$row->Address,
+                    "City"=>$row->City];
+}
+
+$val = [
+   "took" =>222,
+   "code"=>200,
+   "message"=>"Response succesfully",
+   "data"=>$customers
+];
+$output=json_encode($val);
+echo $output;
+
+
+?>
